7e1afad7823180ea504b7bc6ec96990d2d2cad07
jakechaley <jakechaley@gmail.com>
2022-02-28 14:44:55 -0800

    updated file structure

--- ToDoList/program.cs
+++ ToDoList/program.cs
@@ -1,48 +1,20 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace UserInterface
+namespace ToDoList
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to your To Do List!");
-      AddOrViewList();
-    }
-    private static void AddOrViewList()
-    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-      Console.WriteLine("Press 'A' to add an item. Press 'V' to view your list. Press 'E' to exit program");
-      List<Item> newList = new List<Item> {};
-      string stringUserInput = Console.ReadLine();
-      if (stringUserInput == "A" || stringUserInput == "a")
-      {
-        Console.WriteLine("What would you like to add?");
-        string description = Console.ReadLine();
-        Item newItem = new Item (description);
-        AddOrViewList();
-      }
-      else if (stringUserInput == "V" || stringUserInput == "v" )
-      {
-        List<Item> result = Item.GetAll();
-        foreach(Item items in result)
-        {
-          Console.WriteLine(items.Description);
-        }
-        AddOrViewList();
-      }
-      else if (stringUserInput == "E" || stringUserInput == "e")
-      {
-        Console.WriteLine("Goodbye!");
-      }
-      else
-      {
-        Console.WriteLine("We don't recognize that response... Try agian.");
-        AddOrViewList();
-      }
+      host.Run();
     }
-    
   }
 }

2eec0b9e8ceeb1073fab65a46636a6cf53e3e7f4
jakechaley <jakechaley@gmail.com>
2022-03-02 10:29:31 -0800

    allow users to view detailes about specific Item
    Co-Authored-by: Chase Stokes <chasehstokes@gmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -13,8 +13,8 @@ namespace ToDoList.Controllers
       return View(allItems);
     }
 
-    [HttpGet("/item/new")]
-    public ActionResult CreateForm()
+    [HttpGet("/items/new")]
+    public ActionResult New()
     {
       return View();
     }
@@ -31,6 +31,13 @@ namespace ToDoList.Controllers
     {
       Item.ClearAll();
       return View();
+    }
+
+    [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+      Item foundItem = Item.Find(id);
+      return View(foundItem);
     }
   }
 }
